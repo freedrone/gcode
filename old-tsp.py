76751e24a919256ab494edbf734407272ba6ca5d
freedrone <erancihan@gmail.com>
2018-07-27 10:16:21 +0300

    migrate to py3.6 with import opt

--- old-tsp.py
+++ old-tsp.py
@@ -7,73 +7,74 @@
 # ROB 534 Final Project, 3/7/17
 
 from collections import defaultdict
-from node import Node
+from .node import Node
+
 
 # input graph format: ([Node1, Node2, ...], index)
 # output format: (mincost, list) where list is the order of nodes visited in optimal path with mincost
 def tsp(nodes, first_node):
-	cache = defaultdict(lambda : (-1, -1))
+    cache = defaultdict(lambda: (-1, -1))
 
-	# to save space and avoid set operations, I used a bitmap to encode which nodes 
-	# are/aren't included in the "set" visited		
-	visited = 0
-	for i in xrange(len(nodes)):
-		visited |= (1 << i)
-	
-	# try to find a solution starting at every node i that connects to 0
-	# keep best of these solutions and add cost(i, 0)
-	mincost, v, start = float("inf"), -1, -1
-	adj_0 = nodes[first_node].get_edges(0) + nodes[first_node].get_edges(1)
-	for (wi0, vi, starti) in adj_0:
-		#print "checking", vi, wi0
-		costi0, _, start = _tsp(visited, vi, starti, first_node, nodes, cache)
-		#costi0 += wi0
-		if costi0 < mincost:
-			mincost, v, start = costi0, vi, starti
-			
-	if mincost < float("inf"):
-		result = backtrace(visited, v, 1-start, cache)
-		#result.append(0)
-		return (mincost, result)
-	else:
-		return(mincost, [])
-	
+    # to save space and avoid set operations, I used a bitmap to encode which nodes
+    # are/aren't included in the "set" visited
+    visited = 0
+    for i in range(len(nodes)):
+        visited |= (1 << i)
+
+    # try to find a solution starting at every node i that connects to 0
+    # keep best of these solutions and add cost(i, 0)
+    mincost, v, start = float("inf"), -1, -1
+    adj_0 = nodes[first_node].get_edges(0) + nodes[first_node].get_edges(1)
+    for (wi0, vi, starti) in adj_0:
+        # print "checking", vi, wi0
+        costi0, _, start = _tsp(visited, vi, starti, first_node, nodes, cache)
+        # costi0 += wi0
+        if costi0 < mincost:
+            mincost, v, start = costi0, vi, starti
+
+    if mincost < float("inf"):
+        result = backtrace(visited, v, 1 - start, cache)
+        # result.append(0)
+        return (mincost, result)
+    else:
+        return (mincost, [])
+
+
 def _tsp(visited, vi, starti, first_node, nodes, cache, level=0):
-	if (visited, vi, starti) in cache:
-		return cache[(visited, vi, starti)]
-	
-	# base case: if bitmap has only bit 0 set, it is the set containing 0
-	if visited == 2**first_node:
-		cache[(visited, vi, starti)] = 0, -1, -1
-		return cache[(visited, vi, starti)]
-	
-	mincost, j, start = float("inf"), -1, -1
-	remaining = visited - (2**vi)
-	adj_i = nodes[vi].get_edges(1 - starti)
-	for (wij, vj, startj) in adj_i:
-		#print "|"*level,vj, wij, remaining
-		if (remaining >> vj) & 1 == 1: # make sure vj is in set
-			#print "|"*level,"y"
-			cost, _, _ = _tsp(remaining, vj, startj, first_node, nodes, cache, level+1)
-			cost += wij
-			if cost < mincost:
-				mincost, j, start = cost, vj, startj
-		#else:
-			#print "|"*level,"n"
+    if (visited, vi, starti) in cache:
+        return cache[(visited, vi, starti)]
 
-	cache[(visited, vi, starti)] = (mincost, j, start)
-	#print "|"*level,visited, mincost, vi, j
-	return (mincost, j, start)
-	
+    # base case: if bitmap has only bit 0 set, it is the set containing 0
+    if visited == 2 ** first_node:
+        cache[(visited, vi, starti)] = 0, -1, -1
+        return cache[(visited, vi, starti)]
+
+    mincost, j, start = float("inf"), -1, -1
+    remaining = visited - (2 ** vi)
+    adj_i = nodes[vi].get_edges(1 - starti)
+    for (wij, vj, startj) in adj_i:
+        # print "|"*level,vj, wij, remaining
+        if (remaining >> vj) & 1 == 1:  # make sure vj is in set
+            # print "|"*level,"y"
+            cost, _, _ = _tsp(remaining, vj, startj, first_node, nodes, cache, level + 1)
+            cost += wij
+            if cost < mincost:
+                mincost, j, start = cost, vj, startj
+    # else:
+    # print "|"*level,"n"
+
+    cache[(visited, vi, starti)] = (mincost, j, start)
+    # print "|"*level,visited, mincost, vi, j
+    return (mincost, j, start)
+
+
 def backtrace(visited, vi, starti, cache):
-	cost, lastv, startj = cache[(visited, vi, starti)]
-	if lastv > -1:
-		remaining = visited - (2**vi)
-		list = backtrace(remaining, lastv, startj, cache)
-	else:
-		list = []
+    cost, lastv, startj = cache[(visited, vi, starti)]
+    if lastv > -1:
+        remaining = visited - (2 ** vi)
+        list = backtrace(remaining, lastv, startj, cache)
+    else:
+        list = []
 
-	list.append((vi, 1-starti))
-	return list
-
-
+    list.append((vi, 1 - starti))
+    return list
--- reorder-gcode.py
+++ reorder-gcode.py
@@ -1,60 +1,58 @@
-from node import *
-from gcode import *
+from .node import distance
+from .gcode import Gcode
 from time import time
 import sys
 
 if len(sys.argv) == 1:
-	print "Usage: reorder-gcode.py <filename-no-extension>"
+    print("Usage: reorder-gcode.py <filename-no-extension>")
 
 else:
-	infile = sys.argv[1] + '.gcode'
-	outfile = sys.argv[1] + '_reordered.gcode'
+    infile = sys.argv[1] + '.gcode'
+    outfile = sys.argv[1] + '_reordered.gcode'
 
-	gcode = Gcode(infile)
-	layers = gcode.parse()
-	print "\nFound", len(layers), "layers."	
+    gcode = Gcode(infile)
+    layers = gcode.parse()
+    print("\nFound", len(layers), "layers.")
 
-	## Construct Graph ##
-	## (calculate distances between curve endpoints on each layer)
-	curvecounts = []
-	for (i, layer) in enumerate(layers):
-		curvecounts.append(len(layer.getCurves()))
-		layer.fillEdgeMatrix()
+    ## Construct Graph ##
+    ## (calculate distances between curve endpoints on each layer)
+    curvecounts = []
+    for (i, layer) in enumerate(layers):
+        curvecounts.append(len(layer.getCurves()))
+        layer.fillEdgeMatrix()
 
-	print "Curve count stats (curves/layer):"
-	print "Min:", min(curvecounts), "Max:", max(curvecounts), "Avg:", sum(curvecounts) / len(curvecounts)
+    print("Curve count stats (curves/layer):")
+    print("Min:", min(curvecounts), "Max:", max(curvecounts), "Avg:", sum(curvecounts) / len(curvecounts))
 
-	## Original Path Cost ##
-	## (sum of distances between end points in order)
-	original_cost = 0	
-	endpoint = (0, 0) # extrusion start point
-	
-	for (i, layer) in enumerate(layers):
-		for c in layer.getCurves():
-			original_cost += distance(endpoint[0], endpoint[1], c.get_x(0), c.get_y(0))
-			endpoint = (c.get_x(1), c.get_y(1))
-			
-	print "\nOriginal path cost from Gcode:", original_cost
+    ## Original Path Cost ##
+    ## (sum of distances between end points in order)
+    original_cost = 0
+    endpoint = (0, 0)  # extrusion start point
 
-	## Greedy TSP Distance Minimization ##
-	## (minimize distance between space filling curve endpoins)
-	solutions = []
-	solution_cost = 0
-	endpoint = (0, 0) # set this initially to extrusion start point before first layer
-	
-	starttime = time()	
+    for (i, layer) in enumerate(layers):
+        for c in layer.getCurves():
+            original_cost += distance(endpoint[0], endpoint[1], c.get_x(0), c.get_y(0))
+            endpoint = (c.get_x(1), c.get_y(1))
 
-	for (j, layer) in enumerate(layers):
-		mincost, best_path, endpoint = layer.minimizeCost(endpoint, j) # this will call into TSP solver
-		solutions.append((mincost, best_path))
-		solution_cost += mincost
-	
-	print "New path cost from solver:", solution_cost
-	print "Execution time:", time() - starttime, "seconds."
-	
-	## Reorder/Rewrite Gcode File ##
-	gcode.reorder(outfile, solutions)
+    print("\nOriginal path cost from Gcode:", original_cost)
 
-	print "New Gcode written to:", outfile
+    ## Greedy TSP Distance Minimization ##
+    ## (minimize distance between space filling curve endpoins)
+    solutions = []
+    solution_cost = 0
+    endpoint = (0, 0)  # set this initially to extrusion start point before first layer
 
+    starttime = time()
 
+    for (j, layer) in enumerate(layers):
+        mincost, best_path, endpoint = layer.minimizeCost(endpoint, j)  # this will call into TSP solver
+        solutions.append((mincost, best_path))
+        solution_cost += mincost
+
+    print("New path cost from solver:", solution_cost)
+    print("Execution time:", time() - starttime, "seconds.")
+
+    ## Reorder/Rewrite Gcode File ##
+    gcode.reorder(outfile, solutions)
+
+    print("New Gcode written to:", outfile)
